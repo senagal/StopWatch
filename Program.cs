569cb64dc00fd749a663feb33a3509ec95c4397e
senagal <senaabdisa4kilo@gmail.com>
2024-12-16 11:33:59 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        // Subscribing to teh events
+        stopwatch.OnStarted += MessageHandler;
+        stopwatch.OnStopped += MessageHandler;
+        stopwatch.OnReset += MessageHandler;
+
+        Console.WriteLine("Stopwatch Application");
+        Console.WriteLine("Use the following commands:");
+        Console.WriteLine("S - StopwatchStarted");
+        Console.WriteLine("T - StopwatchStopped");
+        Console.WriteLine("R - StopwatchReseted");
+        Console.WriteLine("E - ExitingStopwatch");
+
+        while (true)
+        {
+            Console.Write("Enter command: ");
+            char command = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (char.ToUpper(command))
+            {
+                case 'S':
+                    stopwatch.Start();
+                    break;
+                case 'T':
+                    stopwatch.Stop();
+                    break;
+                case 'R':
+                    stopwatch.Reset();
+                    break;
+                case 'E':
+                    Console.WriteLine("Goodbye.");
+                    stopwatch.Stop();
+                    return;
+                default:
+                    Console.WriteLine("Invalid command, Please retry.");
+                    break;
+            }
+        }
+
+    static void MessageHandler(string message)
+    {
+        Console.WriteLine(message);
+    }
+
---
+++ Stopwatch.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+
+public class Stopwatch
+{
+    public delegate void StopwatchEventHandler(string message);
+    public event StopwatchEventHandler? OnStarted;
+    public event StopwatchEventHandler? OnStopped;
+    public event StopwatchEventHandler? OnReset;
+
+    private TimeSpan TimeElapsed;
+    private bool IsRunning;
+    private Timer? Timer;
+
+    public Stopwatch()
+    {
+        TimeElapsed = TimeSpan.Zero;
+        IsRunning = false;
+    }
+
+    public void Start()
+    {
+        if (IsRunning) return;
+        IsRunning = true;
+        Timer = new Timer(Tick, null, 0, 1000);
+        OnStarted?.Invoke("Stopwatch Started!");
+    }
+
+    public void Stop()
+    {
+        if (!IsRunning) return;
+        IsRunning = false;
+        Timer?.Dispose();
+        OnStopped?.Invoke($"Stopwatch Stopped! Time Elapsed: {TimeElapsed}");
+    }
+
+    public void Reset()
+    {
+        Stop();
+        TimeElapsed = TimeSpan.Zero;
+        OnReset?.Invoke("Stopwatch Reseted!");
+    }
+
+    private void Tick(object? state)
+    {
+        if (IsRunning)
+        {
+            TimeElapsed = TimeElapsed.Add(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"Time Elapsed: {TimeElapsed}");
+        }
+    }
+}
